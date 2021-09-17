82ed6013051b8440dd00b4b5523332ac2acb638d
InoaPSilva <inoa.silva@di2win.com>
2021-09-17 19:45:37 -0300

    build: User route added 🛠

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
+const user = require('../controllers/userController')
+
 app.get("/", (req, res) => {
     res.send("Works goddammit!");
 });
+
+app.post('/user/register', user)
 
 module.exports = router;
