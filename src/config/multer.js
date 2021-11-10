8b9c025bde78e098d5a57df6a4f05c3755de5cb9
InoaPSilva <inoaliberato@gmail.com>
2021-11-10 16:03:59 -0300

    Fix: Fixes on multer.js

--- src/config/multer.js
+++ src/config/multer.js
@@ -8,56 +8,58 @@ const MAX_SIZE_TWO_MEGABYTES = 2 * 1024 * 1024;
 
 const storageTypes = {
   local: multer.diskStorage({
-    destination: (req, file, callback) => {
-      callback(null, path.resolve(__dirname, "..", "..", "tmp", "uploads")); //folder walk - locally saved files
+    destination: (req, file, cb) => {
+      cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
     },
-    filename: (req, file, callback) => {
+    filename: (req, file, cb) => {
       crypto.randomBytes(16, (err, hash) => {
-        if (err) callback(err);
-        // taking the key from the image and joining it with its name and format
+        if (err) cb(err);
+
         file.key = `${hash.toString("hex")}-${file.originalname}`;
 
-        callback(null, file.key);
+        cb(null, file.key);
+        console.log("Local");
+
       });
-    },
+    }
   }),
   s3: multerS3({
     s3: new aws.S3(),
     bucket: process.env.BUCKET_NAME,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: "public-read",
-    key: (req, file, callback) => {
+    key: (req, file, cb) => {
       crypto.randomBytes(16, (err, hash) => {
-        if (err) callback(err);
-        // taking the key from the image and joining it with its name and format
+        if (err) cb(err);
+
         const fileName = `${hash.toString("hex")}-${file.originalname}`;
 
-        callback(null, fileName);
+        cb(null, fileName);
+        console.log("s3");
+
       });
-    },
-  }),
+    }
+  })
 };
 
 module.exports = {
   dest: path.resolve(__dirname, "..", "..", "tmp", "uploads"),
   storage: storageTypes[process.env.STORAGE_TYPE],
   limits: {
-    fileSize: MAX_SIZE_TWO_MEGABYTES,
+    fileSize: MAX_SIZE_TWO_MEGABYTES
   },
   fileFilter: (req, file, cb) => {
-    // allowed formats for submission
     const allowedMimes = [
       "image/jpeg",
       "image/pjpeg",
       "image/png",
-      "image/gif",
+      "image/gif"
     ];
 
-    // checking allowed formats
     if (allowedMimes.includes(file.mimetype)) {
-      callback(null, true);
+      cb(null, true);
     } else {
-      callback(new Error("Invalid file type."));
+      cb(new Error("Invalid file type."));
     }
-  },
+  }
 };
